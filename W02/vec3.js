6f0e4e148c7e3349c1c7984662ee1b0f50455d2b
km <kazuzaka53@gmail.com>
2025-10-09 19:04:54 +0900

    update

--- W02/vec3.js
+++ W02/vec3.js
@@ -19,3 +19,24 @@ class Vec3 {
 		return Math.max(this.x, this.y, this.z);
 	}
 }
+
+function AreaOfTriangle(VecA, VecB, VecC) {
+	const a = Math.sqrt(
+		(VecB.x - VecA.x) ** 2 +
+		(VecB.y - VecA.y) ** 2 +
+		(VecB.z - VecA.z) ** 2
+	);
+	const b = Math.sqrt(
+		(VecC.x - VecB.x) ** 2 +
+		(VecC.y - VecB.y) ** 2 +
+		(VecC.z - VecB.z) ** 2
+	);
+	const c = Math.sqrt(
+		(VecA.x - VecC.x) ** 2 +
+		(VecA.y - VecC.y) ** 2 +
+		(VecA.z - VecC.z) ** 2
+	);
+	const s = (a + b + c) / 2;
+
+	return Math.sqrt(s * (s - a) * (s - b) * (s - c));
+}
